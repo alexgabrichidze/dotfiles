f0b71c4f931292ed37dcc94a426369fcadfa8068
Alex Gabrichidze <74346239+alexgabrichidze@users.noreply.github.com>
2026-05-18 02:01:00 -0500

    fix: update Treesitter config for Neovim 0.12

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,84 +1,90 @@
 -- Treesitter configuration
 
+local ensure_installed = {
+    "lua",
+    "vim",
+    "vimdoc",
+    "query",
+    "bash",
+    "java",
+    "json",
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+    "markdown",
+    "markdown_inline",
+}
+
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
+    branch = "main",
+    lazy = false,
+    build = function()
+        require("nvim-treesitter").install(ensure_installed):wait(300000)
+    end,
     dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
+        {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            branch = "main",
+        },
     },
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- Enable highlighting
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
+        local treesitter = require("nvim-treesitter")
+        treesitter.setup()
 
-            -- Enable indentation
-            indent = {
-                enable = true,
-            },
+        local installed = {}
+        for _, parser in ipairs(treesitter.get_installed("parsers")) do
+            installed[parser] = true
+        end
 
-            -- Auto-install missing parsers
-            auto_install = true,
+        local missing = {}
+        for _, parser in ipairs(ensure_installed) do
+            if not installed[parser] then
+                table.insert(missing, parser)
+            end
+        end
+        if #missing > 0 then
+            treesitter.install(missing)
+        end
 
-            -- Install these language parsers
-            ensure_installed = {
-                "lua",
-                "vim",
-                "bash",
-                "java",
-                "json",
-                "html",
-                "css",
-                "javascript",
-                "typescript",
-            },
+        local group = vim.api.nvim_create_augroup("user_treesitter", { clear = true })
+        vim.api.nvim_create_autocmd("FileType", {
+            group = group,
+            pattern = ensure_installed,
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+                vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
 
-            -- Incremental selection config
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<leader>ss",
-                    node_incremental = "<leader>si",
-                    scope_incremental = "<leader>sc",
-                    node_decremental = "<leader>sd",
-                },
-            },
-
-            -- Text objects config
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        -- Functions
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-
-                        -- Classes
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-
-                        -- Parameters
-                        ["ap"] = "@parameter.outer",
-                        ["ip"] = "@parameter.inner",
-
-                        -- Loops
-                        ["al"] = "@loop.outer",
-                        ["il"] = "@loop.inner",
-
-                        -- Conditionals (e.g., if/else)
-                        ["acond"] = "@conditional.outer",
-                        ["icond"] = "@conditional.inner",
-
-                        -- Blocks (e.g., {} or do/end)
-                        ["ab"] = "@block.outer",
-                        ["ib"] = "@block.inner",
-                    },
-                },
+        require("nvim-treesitter-textobjects").setup({
+            select = {
+                lookahead = true,
             },
         })
+
+        local select = require("nvim-treesitter-textobjects.select")
+        local textobjects = {
+            af = { "@function.outer", "Select function outer" },
+            ["if"] = { "@function.inner", "Select function inner" },
+            ac = { "@class.outer", "Select class outer" },
+            ic = { "@class.inner", "Select class inner" },
+            ap = { "@parameter.outer", "Select parameter outer" },
+            ip = { "@parameter.inner", "Select parameter inner" },
+            al = { "@loop.outer", "Select loop outer" },
+            il = { "@loop.inner", "Select loop inner" },
+            acond = { "@conditional.outer", "Select conditional outer" },
+            icond = { "@conditional.inner", "Select conditional inner" },
+            ab = { "@block.outer", "Select block outer" },
+            ib = { "@block.inner", "Select block inner" },
+        }
+
+        for lhs, mapping in pairs(textobjects) do
+            vim.keymap.set({ "x", "o" }, lhs, function()
+                select.select_textobject(mapping[1], "textobjects")
+            end, { desc = mapping[2] })
+        end
     end,
 }
