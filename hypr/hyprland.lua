11607b5142593fd932e088cc41c04aa1315bbe31
Alex Gabrichidze <74346239+alexgabrichidze@users.noreply.github.com>
2026-08-26 13:26:00 -0500

    refactor: complete Hyprland Lua migration

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,24 +1,15 @@
-local variables = require("config.variables")
+-- Main Hyprland configuration entrypoint
 
-assert(type(variables) == "table")
-assert(type(variables.main_mod) == "string")
-assert(type(variables.apps) == "table")
-assert(type(variables.apps.terminal) == "string")
-
-require("config.monitors")
-
+require("config.variables")
 require("config.environment")
+require("config.monitors")
 
 require("config.layout")
 require("config.appearance")
 require("config.animations")
 
 require("config.input")
-
 require("config.permissions")
-
 require("config.windows")
-
 require("config.bindings")
-
 require("config.autostart")
